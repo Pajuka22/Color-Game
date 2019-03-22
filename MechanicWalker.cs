286b2acc3600466ce282eb68851dd07a91159f03
Kaushik Pahuja <nebula374@gmail.com>
2019-03-22 15:32:10 -0700

    Add files via upload

---
+++ MechanicWalker.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MechanicWalker : MechanicEnemy
+{
+    public enum States { NotWorking, Walking, Running, Transforming, Attacking}
+    public GameObject player;
+    public States currentState = States.NotWorking;
+    public Rigidbody2D RB;
+    public LayerMask enemyMask;
+    float width;
+    Transform myTransform;
+    bool grounded;
+    public float chaseRad = 100;
+    public float distance;
+    public Collider2D thisCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        width = this.GetComponent<SpriteRenderer>().bounds.extents.x;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myTransform = this.transform;
+        Vector2 linecastPos = (myTransform.position - myTransform.right * width);
+        Debug.DrawLine(linecastPos + Vector2.down, linecastPos + Vector2.down + Vector2.down);
+        grounded = Physics2D.Linecast(linecastPos + Vector2.down, linecastPos + Vector2.down + Vector2.down, enemyMask);
+        switch (currentState)
+        {
+            case States.Walking:
+                {
+                    if ((player.transform.position - this.transform.position).magnitude < chaseRad)
+                    {
+                        currentState = States.Transforming;
+                    }
+                    if (!grounded)
+                    {
+                        Vector3 currRot = myTransform.eulerAngles;
+                        currRot.y += 180;
+                        this.transform.eulerAngles = currRot;
+                        if (RB.velocity.x > 0)
+                        {
+                            RB.velocity = new Vector2(-8, RB.velocity.y);
+                        }
+                        else
+                        {
+                            RB.velocity = new Vector2(8, RB.velocity.y);
+                        }
+                    }
+                    else
+                    {
+                        if (RB.velocity.x < 0)
+                        {
+                            RB.velocity = new Vector2(-8, RB.velocity.y);
+                        }
+                        else
+                        {
+                            RB.velocity = new Vector2(8, RB.velocity.y);
+                        }
+                    }
+                }
+                break;
+            case States.Transforming:
+                {
+                    StartCoroutine(Transform(1));
+                }
+                break;
+            case States.Running:
+                {
+                    RB.velocity = new Vector2 (10 * sign(player.transform.position.x - this.transform.position.x), RB.velocity.y);
+                    if ((player.transform.position - this.transform.position).magnitude > chaseRad)
+                    {
+                        currentState = States.Walking;
+                    }
+                }
+                break;
+            default:
+                {
+                    currentState = States.NotWorking;
+                }
+                break;
+        }
+    }
+    public override void ActAwake()
+    {
+        
+    }
+    public override void Activate()
+    {
+        WOKE = true;
+        currentState = States.Walking;
+    }
+    public int sign(float val)
+    {
+        if(val == 0)
+        {
+            return 0;
+        }else if(val < 0)
+        {
+            return -1;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+    IEnumerator Transform(float time)
+    {
+        RB.velocity = new Vector2(0f, 0f);
+        yield return new WaitForSeconds(time);
+        currentState = States.Running;
+    }
+    public void OnCollisionEnter2D(Collision2D col)
+    {
+        
+    }
+}
---
+++ MovementController.cs
@@ -0,0 +1,191 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour {
+
+    public Rigidbody2D RB;
+    public float walkSpeed = 10;
+    [Range(1, 10)]
+    public float acceleration = 1;//how quickly you accelerate when changing direction or starting movement
+    [SerializeField] private LayerMask WhatIsGround;
+    public List<float> jumpHeight = new List<float>();//list of heights of jumps. Allows for as many jumps as you want
+    private int Jumps;//number of jumps the player currently has left.
+    public bool variableJump = false;//can the player vary the height of jumps by releasing the jump button?
+    [Range(1, 2)]
+    public float fallingMult = 1;//how much faster you fall after reaching peak height or releasing jump button (if variableJump)
+    [Range(0, 1)]
+    public float airFriction = 0;
+    public float waterGravMult = 0.5f;
+    public float waterFriction = 0.5f;
+    [SerializeField] private LayerMask WhatIsWater;
+    private Transform Ground;//
+    private Transform Ceiling;//Ground and ceiling checks
+    private bool bGrounded;//grounded?
+    private float GroundRadius = 0.2f;//radius for which it will become grounded
+    public List<float> jumpSpeed = new List<float>();//list of jumpspeeds filled in when the game starts with a program. Guarantees constant jump height
+    [SerializeField] private LayerMask WhatHurts;
+    float initGrav;//initial gravity has to be stored somewhere
+    public bool hashealth = false;
+    public bool haslives = false;
+    public int health = 1;
+    public int lives = 3;
+    public float invincibilityTime = 1;
+    float invincibility = 0;
+    bool inWater = false;
+    public int currentHealth;
+    int currentLives;
+    public Collider2D thisCollider;
+
+    // Use this for initialization
+    void Start () {
+        jumpSpeed.Clear();
+        jumpSpeed.TrimExcess();
+        setJumpSpeed(0, jumpHeight.Count);
+        //fill in jumpspeed list with all the values needed.
+        initGrav = RB.gravityScale;
+        Jumps = jumpSpeed.Count;
+        currentHealth = health;
+        currentLives = lives;
+        //just storing initial values
+
+    }
+    public void setJumpSpeed(int first, int last)
+    {
+        for(int i = first; i < last; i++)
+        {
+            if (jumpSpeed.Count < i + 1)
+            {
+                jumpSpeed.Add(Mathf.Sqrt(2 * RB.gravityScale * jumpHeight[i]));
+            }
+        }
+    }
+    private void Awake()
+    {
+        Ground = transform.Find("Ground");
+        Ceiling = transform.Find("Ceiling");
+        //find these
+    }
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        bGrounded = false;
+        if(Jumps == jumpSpeed.Count)
+        {
+            Jumps--;
+        }
+        //basically says that number of jumps is jumpSpeed.Count - 1 so that if the player runs off a platform, they don't get the grounded jump
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(Ground.position, GroundRadius, WhatIsGround);
+        for(int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i] != gameObject)
+            {
+                bGrounded = true;
+                Jumps = jumpSpeed.Count;
+            }
+        }
+        if (invincibility > 0)
+        {
+            invincibility -= Time.fixedDeltaTime;
+        }
+        //if grounded, get all jumps back
+    }
+    public void Move(float speed, bool jump, bool crouch)
+    {
+        speed *= walkSpeed;
+        //speed is horizontal axis, so multiply by speed. Desired speed, not absolute.
+        if (speed != 0)
+        {
+            if (RB.velocity.x > speed)
+            {
+                speed = RB.velocity.x - acceleration;
+            }
+            if (RB.velocity.x < speed)
+            {
+                speed = RB.velocity.x + acceleration;
+            }
+        }
+        else
+        {
+            if(RB.velocity.x > 0)
+            {
+                speed = RB.velocity.x > acceleration ? RB.velocity.x - acceleration : 0;
+            }
+            if(RB.velocity.x < 0)
+            {
+                speed = Mathf.Abs(RB.velocity.x) > acceleration ? RB.velocity.x + acceleration : 0;
+            }
+        }
+        //acceleration stuff. Makes sure it stops when done accelerating, and accelerates toward desired speed.
+        if (!bGrounded && (RB.velocity.y < 0 || (variableJump && !Input.GetButton("Jump"))))
+        {
+            RB.gravityScale = fallingMult * initGrav * (inWater ? waterGravMult : 1);
+        }
+        //if it's in the air and it's either falling or the jump is variable and the player has released jump, fall faster
+        else
+        {
+            RB.gravityScale = initGrav * (inWater ? waterGravMult : 1);
+        }
+        //if it's grounded or moving upward or the jump isn't variable or the player hasn't released jump, set gravityScale back to initial value
+        RB.velocity = new Vector2(speed, RB.velocity.y);
+        //set horizontal velocity to speed.
+        if (jump && Jumps > 0)
+        {
+            RB.velocity = new Vector2(RB.velocity.x, jumpSpeed[jumpSpeed.Count - Jumps]);
+            Jumps--;
+            //if the player tried to jump and you can jump (Jumps counts jumps left) set the velocity to the jumpsSpeed's value at the jump the player's currently on and lose a jump.
+        }
+        RB.drag = inWater ? waterFriction : airFriction;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if((WhatIsWater.value & 1 << collision.gameObject.layer) == 1 << collision.gameObject.layer)
+        {
+            inWater = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if ((WhatIsWater.value & 1 << col.gameObject.layer) == 1 << col.gameObject.layer)
+        {
+            inWater = false;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (Has<MechanicHurts>(col.gameObject) ? col.gameObject.GetComponent<MechanicHurts>().WOKE : false
+            && invincibility <= 0)
+        {
+            if ((Has<MechanicHurts>(col.gameObject) ? col.collider == col.gameObject.GetComponent<MechanicHurts>().Damager : true))
+            {
+                invincibility = invincibilityTime;
+                if (hashealth)
+                {
+                    currentHealth -= col.gameObject.GetComponent<MechanicHurts>().Damage;
+                }
+                if (health <= 0 || !hashealth)
+                {
+                    currentLives--;
+                    currentHealth = health;
+                }
+                if (currentLives == 0)
+                {
+                    die();
+                }
+            }
+        }
+        if (col.gameObject.tag == "Enemy")
+        {
+            Physics2D.IgnoreCollision(col.collider, col.otherCollider);
+        }
+    }
+    private void die()
+    {
+        //input dying code here.
+    }
+    public bool Has<T>(GameObject GO)
+    {
+        return GO.GetComponent<T>() != null;
+    }
+}
+
