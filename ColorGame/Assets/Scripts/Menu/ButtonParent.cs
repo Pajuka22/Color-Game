ee13892c240e4b27fc5c4a92a878d1e5b7a68912
Pajuka22 <nebula374@gmail.com>
2019-05-02 19:03:40 -0700

    menu setup
    Now there's a menu. Nobody touch it, it took way to long to set up and I refuse to get it working again.

---
+++ ColorGame/Assets/Scripts/Menu/ButtonParent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonParent : MonoBehaviour
+{
+    public enum States { Deselected, Selected, Submitted };
+    public States Current;
+    [SerializeField] Animator anim;
+    public float SubmitTime = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetInteger("State", (int)Current);
+    }
+    public virtual void Select()
+    {
+
+    }
+}
---
+++ ColorGame/Assets/Scripts/Menu/MenuController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField] List<ButtonParent> Buttons;
+    bool keydown = false;
+    public int index = 0;
+    public static bool IsPaused = false;
+    bool submitted = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (!IsPaused)
+        {
+            if (!submitted)
+            {
+                if (Input.GetAxis("Vertical") != 0)
+                {
+                    if (!keydown)
+                    {
+
+                        if (Input.GetAxis("Vertical") < 0)
+                        {
+                            index++;
+                            Buttons[index - 1].Current = ButtonParent.States.Deselected;
+                        }
+                        else
+                        {
+                            index--;
+                            Buttons[index+1].Current = ButtonParent.States.Deselected;
+                            if (index < 0)
+                            {
+                                index = Buttons.Count - 1;
+                            }
+                        }
+                        index = index % Buttons.Count;
+                        Buttons[index].Current = ButtonParent.States.Selected;
+                        keydown = true;
+                    }
+                }
+                else
+                {
+                    keydown = false;
+                }
+            }
+            if (Input.GetButtonDown("Submit"))
+            {
+                Buttons[index].Current = ButtonParent.States.Submitted;
+                //Invoke("Submit", Buttons[index].SubmitTime);
+                //submitted = true;
+            }
+        }
+    }
+    void Submit()
+    {
+        Buttons[index].Select();
+    }
+}
