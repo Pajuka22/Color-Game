0929748f804c54811a0f7643ec9b2ffe725ddb3e
Pajuka22 <nebula374@gmail.com>
2019-05-05 15:49:27 -0700

    draw health
    finally that works

---
+++ ColorGame/Assets/Scripts/UI/Healthbar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Healthbar : MonoBehaviour
+{
+    Vector3 initScale;
+    public MovementController player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initScale = GetComponent<RectTransform>().localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MonoLib.Has<RectTransform>(gameObject))
+        {
+            GetComponent<RectTransform>().localScale = new Vector3(initScale.x * player.currentHealth / player.health, initScale.y * 1, initScale.z * 1);
+        }
+    }
+}
